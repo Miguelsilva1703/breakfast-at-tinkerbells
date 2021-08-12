3c2c305990154d295a254ccd54bf0f20b172ccf1
Miguelsilva1703 <Miguelsilva170393@gmail.com>
2021-08-12 08:19:01 +0100

    4.Cards

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -14,13 +14,13 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem src={img1} text="Explore the hidden waterfall deep inside the Amazon Jungle" label="Ice cream?!" path="/services" />
+                        <CardItem src={img1} text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" label="Ice cream?!" path="/services" />
                         <CardItem src={img2} text="Travel through the Islands of Bali in a Private Cruise" label="Doggy treats" path="/services" />
                     </ul>
                     <ul className="cards__items">
-                        <CardItem src={img3} text="Set Sail in the Atlantic Ocean visiting Uncharted Waters" label="Birthday cakes" path="/services" />
-                        <CardItem src={img4} text="Experience Football on Top of the Himilayan Mountains" label="Impawsible cookies" path="/products" />
-                        <CardItem src={img5} text="Ride through the Sahara Desert on a guided camel tour" label="Cupcakes" path="/sign-up" />
+                        <CardItem src={img3} text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" label="Birthday cakes" path="/services" />
+                        <CardItem src={img4} text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" label="Impawsible cookies" path="/products" />
+                        <CardItem src={img5} text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" label="Cupcakes" path="/sign-up" />
                     </ul>
                 </div>
             </div>
